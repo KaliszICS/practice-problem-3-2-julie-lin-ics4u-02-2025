e59bd865483c14f97b57d3eb2dbeca1c4bed1614
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-04-04 01:37:18 +0000

    :(

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -8,6 +8,9 @@ public class PracticeProblem {
 		if (num < 3) {
 			return -1;
 		}
+		if (num == 3){
+			return 7;
+		}
 		// Minnimum Moves(num) = (2^num) − 1
 		return 2 * towerOfHanoi(num - 1) + 1;
 	}
